6c503b7fe1b76f7aa4fcdf9b45654bc94a00436c
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2026-08-18 04:36:56 -0400

    fix pep8 error

--- sftpretty/__init__.py
+++ sftpretty/__init__.py
@@ -452,7 +452,8 @@ class Connection(object):
             compression = self._cnopts.compression
             if bool(compress) and compression != ('none',):
                 self._transport.get_security_options().compression = compression
-            log.debug(f'Compression: [{self._transport.get_security_options().compression}]')
+            compression = self._transport.get_security_options().compression
+            log.debug(f'Compression: [{compression}]')
             # Set connection digests
             digests = self._config.get('macs') or self._cnopts.digests
             _digests = self._transport.get_security_options().digests
